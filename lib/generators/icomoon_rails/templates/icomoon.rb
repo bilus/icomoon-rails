b9357e2d1c656a0a3b586ed0b836fb3395962e57
removedporn <86824510+removedporn@users.noreply.github.com>
2022-08-03 14:08:12 +0800

    Delete lib directory

--- lib/generators/icomoon_rails/templates/icomoon.rb
+++
@@ -1,9 +0,0 @@
-IcoMoonRails.setup do |config|
-
-  # Config prefix class
-  config.class_prefix = "icon"
-
-  # Config postfix class
-  config.class_postfix = ""
-
-end
--- lib/icomoon_rails/version.rb
+++
@@ -1,3 +0,0 @@
-module IcomoonRails
-  VERSION = "0.1.0"
-end
